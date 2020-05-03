96a1da0d9deadb7663ff4b19f5596c1801b6ddbb
Ajil Abdalqader <ajilqado@gmail.com>
2020-05-03 23:34:29 +0200

    setting-layout

---
+++ src/component/Form.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+    render() {
+        return (
+            <div>
+              
+<article>
+    <h1>Generate Password</h1>
+    
+  <form  className="flex">
+ 
+        <div className="container">
+  
+        <div>
+        <label>Add word </label>
+        </div>
+        <div className="input">
+        <input name="message"  />
+        </div>
+
+       <div className="button">   
+       <p></p>
+        <button  >Shwo me the Message</button>
+        <button >Add new word</button>
+        </div>
+  </div>
+    </form> 
+ 
+</article>
+            </div>
+        )
+    }
+}
